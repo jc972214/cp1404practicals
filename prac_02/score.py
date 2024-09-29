02411e22d4602046de887b2eccb8d745642691d7
jc972214 <thomas.dallimore@my.jcu.edu.au>
2024-09-29 21:37:43 +1000

    refactor score program to use functions

--- prac_02/score.py
+++ prac_02/score.py
@@ -1,11 +1,30 @@
-score = float(input("Enter score: "))
-if score < 0:
-    print("Invalid score")
-elif score > 100:
-    print("Invalid score")
-elif score > 90:
-    print("Excellent")
-elif score > 50:
-    print("Passable")
-else:
-    print("Bad")
+"""Result Checker"""
+import random
+from random import randint
+
+
+def main():
+    score = float(input("Enter score: "))
+    result = get_result(score)
+    print(f"Your result is: {result}")
+    print()
+    random_score = randint(0, 100)
+    print(f"The random score is: {random_score}")
+    random_result = get_result(random_score)
+    print(f"The random result is: {random_result}")
+
+
+def get_result(score):
+    if score < 0:
+        return "Invalid score"
+    elif score > 100:
+        return "Invalid score"
+    elif score > 90:
+        return "Excellent"
+    elif score > 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+main()
