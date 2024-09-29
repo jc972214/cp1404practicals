6c6a151806af0cf3624eb731b97aedba8d274011
jc972214 <thomas.dallimore@my.jcu.edu.au>
2024-09-29 23:07:08 +1000

    Refactor password check to have minimum number of characters

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,8 +1,9 @@
 """Password Checker"""
 
+MINIMUM_LENGTH = 4
 
 def main():
-    password = get_password()
+    password = get_password(MINIMUM_LENGTH)
     print_asterisks(password)
 
 
@@ -10,10 +11,10 @@ def print_asterisks(password):
     print(len(password) * "*")
 
 
-def get_password():
+def get_password(min_length):
     password = input("Enter your password: ")
-    while len(password) == 0:
-        print("Error: Invalid Password")
+    while len(password) < min_length:
+        print(f"Error: Invalid Password. Needs at least {min_length} characters.")
         password = input("Enter your password: ")
     return password
 
