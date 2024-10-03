075409339d68652489da37ef1497080d85cc2d98
jc972214 <thomas.dallimore@my.jcu.edu.au>
2024-10-04 09:50:53 +1000

    Amend structure of program

--- prac_03/randoms.py
+++ prac_03/randoms.py
@@ -1,23 +1,24 @@
 """Random Numbers"""
 
-#Q1.
+# Q1.
 
-#I saw 16
-#The smallest number I could have seen is 5
-#The largest number I could have seen is 20
+# I saw 16
+# The smallest number I could have seen is 5
+# The largest number I could have seen is 20
 
-#Q2.
+# Q2.
 
-#I saw 5
-#The smallest number I could have seen is 3
-#The largest number I could have seen is 9
-#Line 2 could not have produced a 4 as it only allows numbers from 3 to 10 in increments for 2. Meaning the only possible options are 3, 5, 7, or 9.
+# I saw 5
+# The smallest number I could have seen is 3
+# The largest number I could have seen is 9
+# Line 2 could not have produced a 4 as it only allows numbers from 3 to 10 in increments for 2. Meaning the only possible options are 3, 5, 7, or 9.
 
-#Q3.
+# Q3.
 
-#I saw 4.084234088291388
-#The lowest number I could have seen is 2.5
-#The highest number I could have seen is 5.5
+# I saw 4.084234088291388
+# The lowest number I could have seen is 2.5
+# The highest number I could have seen is 5.5
 
 import random
+
 print(random.randint(1, 100))
