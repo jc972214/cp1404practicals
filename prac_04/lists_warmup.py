8b75c0891a3d217cf19211167fccce74bcb3b292
jc972214 <thomas.dallimore@my.jcu.edu.au>
2024-10-11 09:17:07 +1000

    Add lists warmup file

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,29 @@
+"""Warm-up exercise for Practical 4"""
+
+numbers = [3, 1, 4, 1, 5, 9, 2]
+#numbers[0] = 3
+#numbers[-1] = 2
+#numbers[3] = 1
+#numbers[:-1] = [3, 1, 4, 1, 5, 9]
+#numbers[3:4] = [1,5]
+#5 in numbers = True
+#7 in numbers = False
+#"3" in numbers = False
+#numbers + [6, 5, 3] = [[3, 1, 4, 1, 5, 9, 6, 5, 3]
+
+#Q1.
+numbers[0] = "ten"
+print(numbers)
+
+#Q2.
+numbers[-1] = 1
+print(numbers)
+
+#Q3.
+print(numbers[2:])
+
+#Q4.
+print(9 in numbers)
+
+
+
