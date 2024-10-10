ab9596c680500293c0ef111203a4c25363dbf4eb
jc972214 <thomas.dallimore@my.jcu.edu.au>
2024-10-11 09:59:48 +1000

    Modify subject reader to return data as a nested list, and print a display

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -9,21 +9,30 @@ FILENAME = "subject_data.txt"
 def main():
     data = load_data()
     print(data)
+    display_data(data)
 
 
 def load_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
+    subject = []
     input_file = open(FILENAME)
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
+        # print(line)  # See what a line looks like
+        # print(repr(line))  # See what a line really looks like
         line = line.strip()  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
+        # print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+        # print(parts)  # See if that worked
+        #print("----------")
+        subject.append(parts)
+
     input_file.close()
+    return subject
+
+def display_data(data):
+    for datum in data:
+        print(f"{datum[0]} is taught by {datum[1]} and has {datum[2]} students.")
 
 
 main()
