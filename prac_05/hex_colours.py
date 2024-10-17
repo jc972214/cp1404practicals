0bd1fd7dbd3a5847b4f60f30bf74a7a8eceedca0
jc972214 <thomas.dallimore@my.jcu.edu.au>
2024-10-18 09:51:30 +1000

    Adjust formating on hex colours file

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -1,16 +1,15 @@
 """Hex to Colour Calculator"""
 
 COLOUR_TO_HEX = {"Amethyst": "#9966cc",
-    "Aqua": "#00ffff",
-    "Aureolin": "#fdee00",
-    "Baby Blue": "#89cff0",
-    "Baby Pink": "#f4c2c2",
-    "Gray21": "#363636",
-    "LightSalmon4": "#8b5742",
-    "Shamrock Green": "#009e60",
-    "Tart Orange": "#fb4d46",
-    "Zomp": "#39a78e"}
-
+                 "Aqua": "#00ffff",
+                 "Aureolin": "#fdee00",
+                 "Baby Blue": "#89cff0",
+                 "Baby Pink": "#f4c2c2",
+                 "Gray21": "#363636",
+                 "LightSalmon4": "#8b5742",
+                 "Shamrock Green": "#009e60",
+                 "Tart Orange": "#fb4d46",
+                 "Zomp": "#39a78e"}
 
 colour_name = input("Enter colour name: ")
 while colour_name != "":
@@ -19,4 +18,3 @@ while colour_name != "":
     except KeyError:
         print("Invalid Colour")
     colour_name = input("Enter colour name: ")
-
