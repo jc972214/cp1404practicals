8b42f7e407c379bcc782514bef5ed0fb3e7a773b
jc972214 <thomas.dallimore@my.jcu.edu.au>
2024-10-18 10:53:16 +1000

    Complete word occurrences exercise

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -1,5 +1,23 @@
 """
 Word Occurrences
 Estimate: 15 minutes
-Actual:
+Actual: 26 minutes
 """
+
+word_to_count = {}
+user_phrase = input("Text: ").lower().split()
+for word in user_phrase:
+    if word in user_phrase:
+        if word in word_to_count:
+            word_to_count[word] += 1
+        else:
+            word_to_count[word] = 1
+
+user_phrase = list(word_to_count.keys())
+user_phrase.sort()
+
+longest_word = len(max(user_phrase, key = len))
+
+for word in user_phrase:
+    print(f"{word:{longest_word}} : {word_to_count[word]}")
+
