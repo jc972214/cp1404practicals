16a2a1b3daa71e1af6066cce13e718ca430e4f43
jc972214 <thomas.dallimore@my.jcu.edu.au>
2024-11-19 13:48:28 +1000

    Complete conversion of user inputted miles to kilometres when button is pressed

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -1,11 +1,28 @@
 from kivy.app import App
 from kivy.lang import Builder
 
+MILES_TO_KM = 1.60934
 
 class ConvertMilesKm(App):
     def build(self):
         self.title = "Convert Miles to Kilometres"
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
+
+    def get_valid_miles(self):
+        """Receives input miles from user and checks that it is a valid input"""
+        try:
+            miles = float(self.root.ids.user_input.text)
+            return miles
+        except ValueError:
+            return 0
+
+    def handle_conversion(self):
+        """Converts miles to kilometres when button is pressed"""
+        miles = self.get_valid_miles()
+        conversion = miles * MILES_TO_KM
+        self.root.ids.output_label.text = str(conversion)
+
+
 
 ConvertMilesKm().run()
